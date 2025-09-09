23f0fff4c1264aa31a07d3ee30dfe28831acdca9
KaioFerreira1 <kaiof2902@gmail.com>
2025-09-08 23:45:04 -0300

    inicialização do projeto

---
+++ src/test/java/com/vitalcajavet/msagendamentoconsultas/AgendamentoConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalcajavet.msagendamentoconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
