f4e4556ccffc3acc673a910778f87beef980fc9c
KaioFerreira1 <kaiof2902@gmail.com>
2025-09-14 01:21:20 -0300

    configura WebClient para comunicação com serviço de autenticação

---
+++ src/main/java/com/vitalcajavet/msagendamentoconsultas/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.vitalcajavet.msagendamentoconsultas.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${auth.service.url}")
+    private String authServiceUrl;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(authServiceUrl)
+                .defaultHeader("ngrok-skip-browser-warning", "true")
+                .build();
+    }
+}
